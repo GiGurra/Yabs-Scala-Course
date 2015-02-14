d304fd97ee2e9ad9b891b82b23d6c7164904c595
Johan Kjölhede <kjolhede@telia.com>
2015-02-14 23:56:39 +0100

    cleanup

--- src/main/scala/se/yabs/aichallenge/util/FileUtil.scala
+++ src/main/scala/se/yabs/aichallenge/util/FileUtil.scala
@@ -6,14 +6,7 @@ import scala.reflect.io.Path.string2path
 object FileUtil {
 
   def textFile2String(fileName: String): Option[String] = {
-    if (exists(fileName)) {
-      val source = scala.io.Source.fromFile(fileName)
-      val lines = source.mkString
-      source.close()
-      Some(lines)
-    } else {
-      None
-    }
+    File(fileName).safeSlurp()
   }
 
   def exists(fileName: String): Boolean = {

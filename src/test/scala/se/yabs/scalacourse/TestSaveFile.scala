0781ffd79c2da73402bb2f3cceb8b900762d1b93
Johan Kjölhede <kjolhede@telia.com>
2015-02-14 20:17:23 +0100

    better named random filenames

--- src/test/scala/se/yabs/scalacourse/TestSaveFile.scala
+++ src/test/scala/se/yabs/scalacourse/TestSaveFile.scala
@@ -3,5 +3,12 @@ package se.yabs.scalacourse
 import scala.util.Random
 
 object TestSaveFile {
-  def random() = s"target/${Random.nextString(10)}.json"
+
+  val RAND_LETTERS = "rragheigugmnxcblkadjfghwetiopyiqh".toSeq
+
+  def randLetter() = RAND_LETTERS(Random.nextInt(RAND_LETTERS.size))
+
+  def randString(n: Int) = (0 until n).map(_ => randLetter()).mkString
+
+  def random() = s"target/${randString(10)}.json"
 }

6dfe44bb60e6ff8a6d63699559bca2520a2361d6
Johan Kjölhede <johan.kjolhede@yabs.se>
2015-02-14 01:26:28 +0100

    Implemented ScorePrinter

---
+++ src/main/scala/se/yabs/aichallenge/battleship/ScorePrinter.scala
@@ -0,0 +1,61 @@
+package se.yabs.aichallenge.battleship
+
+import se.yabs.aichallenge.client.serialization.DbSaver
+import scala.collection.mutable.HashMap
+import scala.collection.JavaConversions._
+
+object ScorePrinter {
+
+  def file2String(fileName: String): String = {
+    val source = scala.io.Source.fromFile(fileName)
+    val lines = source.mkString
+    source.close()
+    lines
+  }
+  
+  class UserScore {
+    private var _wins: Int = 0
+    private var _games: Int = 0
+    
+    def losses = games - wins
+    
+    def wins = _wins
+    
+    def games = _games
+    
+    def addLoss() {
+      _games += 1
+    }
+    
+    def addWin() {
+      _games += 1
+      _wins += 1
+    }
+    
+    def winRatio = wins.toFloat / games.toFloat
+    
+  }
+
+  def main(args: Array[String]) {
+    val userDb = DbSaver.read(file2String("game_results.json"))
+    
+    val scores = new HashMap[String, UserScore]    
+    
+    for (gameResult <- userDb.getGames) {
+      if (gameResult.getWinners.size == 1 && gameResult.getPlayers.size == 2) {
+         val winner = gameResult.getWinners.head
+         val loser = (gameResult.getPlayers - winner).head
+         scores.getOrElseUpdate(winner, new UserScore).addWin()
+         scores.getOrElseUpdate(loser, new UserScore).addLoss()
+      } else {
+        throw new RuntimeException("Invalid game result")
+      }
+    }
+    
+    println("[name] | <wins> - <losses> |  <ratio>")
+    for ((name, score) <- scores) {
+      println(s"[$name] | ${score.wins} - ${score.losses} | ${score.winRatio} ")
+    }
+    
+  }
+}

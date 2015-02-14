5d885879a3580f3088831e67e57635566ac26644
Johan Kjölhede <johan.kjolhede@yabs.se>
2015-02-14 01:46:55 +0100

    Better ScorePrinter

--- src/main/scala/se/yabs/aichallenge/battleship/ScorePrinter.scala
+++ src/main/scala/se/yabs/aichallenge/battleship/ScorePrinter.scala
@@ -12,50 +12,54 @@ object ScorePrinter {
     source.close()
     lines
   }
-  
+
   class UserScore {
     private var _wins: Int = 0
     private var _games: Int = 0
-    
+
     def losses = games - wins
-    
+
     def wins = _wins
-    
+
     def games = _games
-    
+
     def addLoss() {
       _games += 1
     }
-    
+
     def addWin() {
       _games += 1
       _wins += 1
     }
-    
+
     def winRatio = wins.toFloat / games.toFloat
-    
+
   }
 
   def main(args: Array[String]) {
+
     val userDb = DbSaver.read(file2String("game_results.json"))
-    
-    val scores = new HashMap[String, UserScore]    
-    
+    val scores = new HashMap[String, UserScore]
+
     for (gameResult <- userDb.getGames) {
       if (gameResult.getWinners.size == 1 && gameResult.getPlayers.size == 2) {
-         val winner = gameResult.getWinners.head
-         val loser = (gameResult.getPlayers - winner).head
-         scores.getOrElseUpdate(winner, new UserScore).addWin()
-         scores.getOrElseUpdate(loser, new UserScore).addLoss()
+        val winner = gameResult.getWinners.head
+        val loser = (gameResult.getPlayers - winner).head
+        scores.getOrElseUpdate(winner, new UserScore).addWin()
+        scores.getOrElseUpdate(loser, new UserScore).addLoss()
       } else {
         throw new RuntimeException("Invalid game result")
       }
     }
-    
-    println("[name] | <wins> - <losses> |  <ratio>")
-    for ((name, score) <- scores) {
-      println(s"[$name] | ${score.wins} - ${score.losses} | ${score.winRatio} ")
+
+    def printScoreLine(name: String, wins: String, losses: String, ratio: String) {
+      println(s"[${name.padTo(10, ' ')}]    |    ${wins.padTo(6, ' ')} -   ${losses.padTo(6, ' ')}    |    ${ratio.padTo(6, ' ')}")
     }
-    
+
+    printScoreLine("name", "wins", "losses", "ratio")
+    for ((name, score) <- scores) {
+      printScoreLine(name, score.wins.toString, score.losses.toString, score.winRatio.toString)
+    }
+
   }
 }

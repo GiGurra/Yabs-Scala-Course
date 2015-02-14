f3100df69135a6923e49adf264d8ebec64572d13
Johan Kjölhede <kjolhede@telia.com>
2015-02-14 21:56:37 +0100

    cleanup

--- src/main/scala/se/yabs/aichallenge/battleship/BattleshipClient.scala
+++ src/main/scala/se/yabs/aichallenge/battleship/BattleshipClient.scala
@@ -5,7 +5,6 @@ import scala.annotation.tailrec
 import se.yabs.aichallenge.ErrorMessage
 import se.yabs.aichallenge.GameChallengeFound
 import se.yabs.aichallenge.GameSelection
-import se.yabs.aichallenge.Message
 import se.yabs.aichallenge.WelcomeMessage
 import se.yabs.aichallenge.client.GameClient
 import se.yabs.aichallenge.host.GameHost
@@ -16,29 +15,25 @@ class BattleshipClient(name: String, pw: String, zmqAddr: String, ai: => Battles
   def this(name: String, pw: String, host: GameHost, ai: => BattleshipAi) = this(name, pw, "127.0.0.1", host.port, ai)
 
   private val aiCtor = () => ai
-  private val gameClient = new GameClient(name, pw, zmqAddr)
-  gameClient.checkin()
+  private val client = new GameClient(name, pw, zmqAddr)
+  client.checkin()
 
   ///////////////////////////////////////////////////////////////////
   /////   API 
 
-  def getNewMessages(pollTimeMillis: Int): Seq[Message] = {
-    gameClient.getNewMessages(pollTimeMillis)
-  }
-
   def playGame(): GameOver = {
     val ai = aiCtor()
-    gameClient.playGame(GameSelection.BATTLESHIP)
+    client.playGame(GameSelection.BATTLESHIP)
 
     for (
-      msgs <- Stream.continually(getNewMessages(10));
+      msgs <- Stream.continually(client.getNewMessages(10));
       msg <- msgs
     ) {
       msg match {
         case msg: GameChallengeFound =>
         case msg: WelcomeMessage     => println(msg.getMsg)
-        case msg: PlaceShipsRequest  => gameClient.send(new PlaceShips(ai.placeShips()))
-        case msg: MakeShotRequest    => gameClient.send(new MakeShot(ai.makeShot()))
+        case msg: PlaceShipsRequest  => client.send(new PlaceShips(ai.placeShips()))
+        case msg: MakeShotRequest    => client.send(new MakeShot(ai.makeShot()))
         case msg: GameOver           => return msg
         case msg: ShotResult         => ai.shotFired(msg.getShooterName, msg.getShot.getPos, msg.getShot.getIsHit)
         case msg: ErrorMessage       => throw new RuntimeException(s"ErrorMessage received from server: ${msg.getMsg}")
@@ -50,7 +45,7 @@ class BattleshipClient(name: String, pw: String, zmqAddr: String, ai: => Battles
   }
 
   def close() {
-    gameClient.close()
+    client.close()
   }
 
 }
